b3a7d70abbfdef4a57611d4059860db68e05b1e8
jihanurrahman33 <jihanurrahman33@gmail.com>
2025-03-06 23:22:58 +0600

    splash screen ui in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/app.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const TaskManagerApp());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp();
-  }
-}
